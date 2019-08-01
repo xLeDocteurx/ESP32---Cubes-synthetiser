9f62ac6c865f6db914dca10c32e8c3ee9d2a40b6
xLeDocteurx <lenoirc.fr@gmail.com>
2019-08-01 22:43:37 +0200

    draft for the loop section

--- ESP32-syntheticubes.ino
+++ ESP32-syntheticubes.ino
@@ -13,16 +13,13 @@ namespace Synth {
   const int resolution = 8;
   const int pin = 5;
   const int channel = 0;
-  
-  int freq = 0;
-  int value = 0;
 
   enum Waveform {sine, triangle, square, saw, sawTouth, whiteNoise};
   enum Envelope {shortPeak, shortPeakWithSustain, longPeak, longPeakWithSustain, sustain};
 
-  Waveform form;
+  Waveform form = Waveform::square;
   int sineValues[32 * resolution];
-  int tringleValues[32 * resolution];
+  int triangleValues[32 * resolution];
   int squareValues[32 * resolution];
   int sawValues[32 * resolution];
   int whiteNoiseValues[32 * resolution];
@@ -85,14 +82,29 @@ void setup() {
 
   Synth::initiateWaveforms();
   
-   for(int i = 0; i < sizeof(Synth::squareValues) / sizeof(Synth::squareValues[0]); i++) {
-       Serial.println(Synth::squareValues[i]);
-   }
+   // for(int i = 0; i < sizeof(Synth::squareValues) / sizeof(Synth::squareValues[0]); i++) {
+   //   Serial.println(Synth::squareValues[i]);
+   // }
   
   Serial.println("Device started properly");
 }
 
+int waveIndex = 0;
+
 void loop() {
   
-  // dacWrite(Synth::pin, Synth::value);
+  if(Synth::form == Synth::Waveform::sine) {
+    dacWrite(Synth::pin, Synth::sineValues[waveIndex]);
+  } else if(Synth::form == Synth::Waveform::triangle) {
+    dacWrite(Synth::pin, Synth::triangleValues[waveIndex]);
+  } else if(Synth::form == Synth::Waveform::square) {
+    dacWrite(Synth::pin, Synth::squareValues[waveIndex]);
+  } else if(Synth::form == Synth::Waveform::saw) {
+    dacWrite(Synth::pin, Synth::sawValues[waveIndex]);
+  } else if(Synth::form == Synth::Waveform::whiteNoise) {
+    dacWrite(Synth::pin, Synth::whiteNoiseValues[waveIndex]);
+  }
+
+  waveIndex = waveIndex >= 255 ? 0 : waveIndex + 1;
+  
 }
